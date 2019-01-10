e011c86a0ce805ef1e048587644c5a2ca94ae68a
timothy-spencer <timothy.spencer@gsa.gov>
2019-01-10 15:14:05 -0800

    changed database names, normalized how it was passed in to the app

--- terraform/google_sql.tf
+++ terraform/google_sql.tf
@@ -1,6 +1,6 @@
 // Postgres Database
-resource "google_sql_database_instance" "gcp-pilot-sql-postgres" {
-  name = "gcppilot-postgres-example"
+resource "google_sql_database_instance" "rails-example" {
+  name = "rails-example"
   database_version = "POSTGRES_9_6"
   region = "${var.region}"
 
@@ -17,7 +17,7 @@ resource "random_string" "postgres_password" {
 resource "google_sql_user" "postgres" {
   name     = "postgres"
   password = "${random_string.postgres_password.result}"
-  instance = "${google_sql_database_instance.gcp-pilot-sql-postgres.name}"
+  instance = "${google_sql_database_instance.rails-example.name}"
 }
 
 output "postgres_password" {
@@ -28,4 +28,9 @@ output "postgres_password" {
 output "postgres_username" {
   value = "${google_sql_user.postgres.name}"
   description = "Postgres username"
+}
+
+output "postgres_instance" {
+  value = "${google_sql_database_instance.rails-example.connection_name}"
+  description = "Postgres instance ID"
 }
