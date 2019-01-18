12b45a485ca52a9af5abc4a69236ebc7c1c90f4d
timothy-spencer <timothy.spencer@gsa.gov>
2019-01-18 11:12:19 -0800

    made blogs be the default controller for most stuff

--- dotnet-example/Startup.cs
+++ dotnet-example/Startup.cs
@@ -62,7 +62,7 @@ namespace dotnet_example
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Blogs}/{action=Index}/{id?}");
             });
         }
     }
